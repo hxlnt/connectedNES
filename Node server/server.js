4d433d93fe71d7866d40e3b0b4aa7c899d6a98c2
Rachel Simone Weil <partytimehexcellent@gmail.com>
2016-05-16 13:13:01 -0500

    Update server.js

--- Node server/server.js
+++ Node server/server.js
@@ -104,7 +104,6 @@ function splitTweet(x){
     line6 = ' ';
     var tweetlines = [line1, line2, line3, line4, line5, line6];
     //// Replace emoji and unrecognized characters outside of basic ASCII with a '?', show ampersands properly
-    x = x.replace(/[\u2019]/g, ''');
     x = x.replace(/([\u007F-\uFF8F])/g, '?');
     x = x.replace(/&amp;/g, '&');
     //// Figure out how to cleverly break a tweet into six lines with no more than 24 characters per line. Deals with edge cases like very long words or tweets that must be truncated.

52f04d486f92364ad23bd97c35a3439c45a79194
Rachel Simone Weil <partytimehexcellent@gmail.com>
2016-05-16 11:33:17 -0500

    Bug fixes: 'F', '&', 12PM
    This update addresses all known bugs. Of course, there could be others... :)

--- Node server/server.js
+++ Node server/server.js
@@ -40,7 +40,7 @@ stream.on('tweet', function(json) {
             hour = hour + 12;
             ampm = 'P';
         }
-        else if (hour > 12) {
+        else if (hour >= 12) {
             hour = hour - 12;
             ampm = 'P';
         }
@@ -100,8 +100,9 @@ function splitTweet(x){
     line5 = ' ';
     line6 = ' ';
     var tweetlines = [line1, line2, line3, line4, line5, line6];
-    //// Replace emoji and unrecognized characters outside of basic ASCII with a '?'
-    x = x.replace(/([\u007F-\uFF8FF])/g, '?');
+    //// Replace emoji and unrecognized characters outside of basic ASCII with a '?', show ampersands properly
+    x = x.replace(/([\u007F-\uFF8F])/g, '?');
+    x = x.replace(/&amp;/g, '&');
     //// Figure out how to cleverly break a tweet into six lines with no more than 24 characters per line. Deals with edge cases like very long words or tweets that must be truncated.
     var tweetlength = x.length;
     if (tweetlength <= 24) {

c88b1f8915e6750ab0c6c8df4062ad5c2cc086a4
Rachel Simone Weil <partytimehexcellent@gmail.com>
2016-07-28 22:38:44 -0500

    Update server.js

--- Node server/server.js
+++ Node server/server.js
@@ -19,7 +19,7 @@ particle.login({username: process.env.mysparkemail, password: process.env.myspar
 
 
 // Check twitter streaming API for these words
-var stream = T.stream('statuses/filter', { track: ['connectedNES','oscon'] })
+var stream = T.stream('statuses/filter', { track: ['connectedNES','nodebots'] })
 
 
 // Parse incoming tweets
